a61d89c7817cc073309a3e09e2a1b663a6f8ee64
Justin Brubaker <justinbrubaker7@gmail.com>
2021-04-12 20:54:18 -0700

    made routes for /api and reservations

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+var router = require('./router/api-router.js')
+
 
 
 const app = express();
@@ -7,11 +9,16 @@ var PORT = process.env.PORT || 5050;
 
 
 //middleware
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', router);
 
 
-//post listening
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+
 app.listen(PORT, () => console.log(`app is running on ${PORT}`));
 
 
